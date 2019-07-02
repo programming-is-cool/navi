8c01cbd889decb0f9b4e270ef8602f5140ae847f
programming-is-cool <ralph13@me.com>
2019-07-02 16:22:25 -0400

    Added some input checks and utilized the switch statement for command checks.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { discordBotToken, eBayAppID } = require('./config.json')
+const { prefix, discordBotToken, eBayAppID } = require('./config.json')
 const ebayCommand = require('./commands').ebayCommand
 const completedListingsRequest = require('./eBayApi/findingReq').completedListingsRequest
 const getSold = require('./eBayApi/utils/parsing').getSold
@@ -12,34 +12,43 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg) => {
-    if(msg.content.startsWith(ebayCommand)) {
-        let keyword = msg.content.slice(ebayCommand.length);
-        completedListingsRequest(eBayAppID, keyword)
-        .then((data) => {
-            const statusRes = data.findCompletedItemsResponse[0].ack[0];
-            const listingQty = data.findCompletedItemsResponse[0].searchResult[0]['@count'];
-            if (statusRes === 'Success' && listingQty > '0') {
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+    const args = msg.content.split(/ +/);
+    const command = args.shift();
+    
+    switch (command) {
+        case ebayCommand:
+            if (!args.length) return msg.reply("You didn't provide any search terms.");
 
-                const itemsList = data.findCompletedItemsResponse[0].searchResult[0].item;
-                const soldList = getSold(itemsList);
-                const avgSalesPrice = calculations.salePriceAverage(soldList);
-                const soldPct = calculations.getSoldPct(soldList, listingQty);
-
-                msg.reply(
-                    `Item quantity: ${ listingQty }
-                    Average sale price: \$${ avgSalesPrice } 
-                    Sold Percentage: ${ soldPct }%`
-                );
-
-            } else if (statusRes === 'Failure' || listingQty === '0') {
-                msg.reply(
-                    'No information from eBay was retrieved.  Check your keywords and try again.'
-                );
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            completedListingsRequest(eBayAppID, args)
+            .then((data) => {
+                const statusRes = data.findCompletedItemsResponse[0].ack[0];
+                const listingQty = data.findCompletedItemsResponse[0].searchResult[0]['@count'];
+                if (statusRes === 'Success' && listingQty > '0') {
+    
+                    const itemsList = data.findCompletedItemsResponse[0].searchResult[0].item;
+                    const soldList = getSold(itemsList);
+                    const avgSalesPrice = calculations.salePriceAverage(soldList);
+                    const soldPct = calculations.getSoldPct(soldList, listingQty);
+    
+                    msg.reply(
+                        `Item quantity: ${ listingQty }
+                        Average sale price: \$${ avgSalesPrice } 
+                        Sold Percentage: ${ soldPct }%`
+                    );
+    
+                } else if (statusRes === 'Failure' || listingQty === '0') {
+                    msg.reply(
+                        'No information from eBay was retrieved.  Check your keywords and try again.'
+                    );
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            break;
+        default:
+            return msg.reply("I do not recognize that command.")
     }
 });
 
